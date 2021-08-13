f5df87a0fd9fbfd31e2871b875fbd600d80e6e59
kmangub <karlomangubat92@gmail.com>
2021-08-13 08:17:06 -0700

    removed unused code

--- birthday_bot.py
+++ birthday_bot.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
-from selenium.common.exceptions import NoSuchElementException
 import time
 import getpass
 
